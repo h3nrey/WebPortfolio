5d17b55faddace60ac02b803424043443d09a984
h3nrey <pedro.novaes.education@gmail.com>
2023-07-10 12:41:20 -0300

    adding a new project: twitch clone

--- src/app/data.ts
+++ src/app/data.ts
@@ -30,29 +30,36 @@ export const skills = {
 }
 
 export const projects = [
-    {
+      {
         id: 1,
+        title: "Twitch Clone",
+        tecnologies: ["NEXT", "TS", "Tailwind", "LocalStorage", "Phosphor"],
+        thumbnail: "https://static.wikia.nocookie.net/liberproeliis/images/2/28/JoJo%E2%80%99s-Bizarre-04.jpg/revision/latest?cb=20161128222812&path-prefix=pt-br",
+        url: "https://github.com/h3nrey/twitch_clone"
+    },
+    {
+        id: 2,
         title: "Find Your Morty",
         tecnologies: ["React", "Js", "Tailwind", "Axios", "radix"],
         thumbnail: "https://static.wikia.nocookie.net/liberproeliis/images/2/28/JoJo%E2%80%99s-Bizarre-04.jpg/revision/latest?cb=20161128222812&path-prefix=pt-br",
         url: "https://github.com/h3nrey/FindYourMorty"
     },
     {
-        id: 2,
+        id: 3,
         title: "FindYourDuo",
         tecnologies: ["React", "Ts", "Tailwind", "Axios", "NodeJs", "Radix", "Prisma", "Express"],
         thumbnail: "https://static.wikia.nocookie.net/liberproeliis/images/2/28/JoJo%E2%80%99s-Bizarre-04.jpg/revision/latest?cb=20161128222812&path-prefix=pt-br",
         url: "https://github.com/h3nrey/FindYourDuo"
     },
     {
-        id: 2,
+        id: 4,
         title: "Ghibli Cinema",
         tecnologies: ["Js", "EJS", "SASS", "NodeJs", "Axios"],
         thumbnail: "https://static.wikia.nocookie.net/liberproeliis/images/2/28/JoJo%E2%80%99s-Bizarre-04.jpg/revision/latest?cb=20161128222812&path-prefix=pt-br",
         url: "https://github.com/h3nrey/GhibliCinema"
     },
     {
-        id: 2,
+        id: 5,
         title: "PokeCatalog",
         tecnologies: ["Js", "EJS", "SASS", "NodeJs", "Axios"],
         thumbnail: "https://static.wikia.nocookie.net/liberproeliis/images/2/28/JoJo%E2%80%99s-Bizarre-04.jpg/revision/latest?cb=20161128222812&path-prefix=pt-br",
